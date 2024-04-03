69d1e84bdf099308d44948fea20a3cb85c2e861c
leehendrick <imhendrick16@gmail.com>
2024-04-03 12:28:40 +0100

    passing nav parameters

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,25 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('inscricoes', function () {
+   return Inertia::render('LandingPage/Inscricoes', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+   ]);
+})->name('page.inscricoes');
+
+Route::get('contatos', function () {
+   return Inertia::render('LandingPage/Contatos', [
+       'canLogin' => Route::has('login'),
+       'canRegister' => Route::has('register'),
+       'laravelVersion' => Application::VERSION,
+       'phpVersion' => PHP_VERSION,
+   ]);
+})->name('page.contatos');
+
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
