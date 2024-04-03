6ab46bfb67c0485d3446c87a5605d6da528a362b
leehendrick <imhendrick16@gmail.com>
2024-04-03 12:29:38 +0100

    add new landing-page components

---
+++ resources/js/Pages/LandingPage/Contatos.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+import LandingHeader from "@/Components/LandingHeader.vue";
+</script>
+
+<template>
+  <landing-header :canLogin="$page.props.canLogin" :canRegister="$page.props.canRegister" :laravelVersion="$page.props.laravelVersion" :phpVersion="$page.props.phpVersion" />
+</template>
+
+<style scoped>
+
+</style>
