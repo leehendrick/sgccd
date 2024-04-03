a8f2ebde75e61d1026ef4681fe9f128ed74f3e11
leehendrick <imhendrick16@gmail.com>
2024-04-03 10:57:02 +0100

    refactoring tailwind template

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -28,7 +28,14 @@ import {onMounted, ref} from 'vue'
   LockClosedIcon,
   XMarkIcon,
 } from '@heroicons/vue/24/outline'
-  import {CheckIcon} from '@heroicons/vue/20/solid'
+import {
+  AcademicCapIcon,
+  BookOpenIcon,
+  BuildingOffice2Icon,
+  CheckBadgeIcon,
+  CheckIcon,
+  UserIcon
+} from '@heroicons/vue/20/solid'
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
 
@@ -40,31 +47,31 @@ import SecondaryButton from "@/Components/SecondaryButton.vue";
   ]
   const features = [
   {
-    name: 'Push to deploy',
-    description:
-    'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates',
-    description:
-    'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Simple queues',
-    description:
-    'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-    icon: ArrowPathIcon,
-  },
-  {
-    name: 'Advanced security',
-    description:
-    'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-    icon: FingerPrintIcon,
-  },
+        name: 'Formadores Qualificados',
+        description:
+        'No coração de qualquer experiência educacional excepcional está a qualidade e a dedicação dos formadores. Em nossos laboratórios, temos uma equipe de instrutores qualificados.',
+        icon: AcademicCapIcon,
+      },
+      {
+        name: 'Ensino de Qualidade',
+        description:
+        'Acreditamos que a educação é a chave para o sucesso pessoal e profissional, e é por isso que nos dedicamos a oferecer uma experiência excepcional em todos os cursos que oferecemos.',
+        icon: CheckBadgeIcon,
+      },
+      {
+        name: 'Aprendizado para Empresas',
+        description:
+        'Em nossos laboratórios, não nos limitamos apenas a atender às necessidades individuais de aprendizado; também fornecemos soluções adaptadas às demandas e objetivos das empresas.',
+        icon: BuildingOffice2Icon,
+      },
+      {
+        name: 'Variedade de cursos',
+        description:
+        'Nos orgulhamos de oferecer uma ampla variedade de cursos projetados para atender a uma ampla gama de interesses, objetivos e níveis de habilidade.',
+        icon: BookOpenIcon,
+      },
   ]
-  const footerNavigation = {
+const footerNavigation = {
   solutions: [
     {name: 'Hosting', href: '#'},
     {name: 'Data Services', href: '#'},
@@ -93,9 +100,48 @@ import SecondaryButton from "@/Components/SecondaryButton.vue";
 
 const mobileMenuOpen = ref(false)
 
+document.addEventListener('DOMContentLoaded', function() {
+  var texts = document.querySelectorAll('.text');
 
-const typedText = ref("Cursos de Curta Duração");
+  function checkVisibility() {
+    texts.forEach(function(text) {
+      if (isElementInViewport(text)) {
+        text.classList.add('visible');
+      }
+    });
+  }
 
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  window.addEventListener('scroll', checkVisibility);
+  checkVisibility(); // Execute a função ao carregar a página
+});
+
+const cursos = ref([
+  {
+    titulo: 'Curso de Desenvolvimento Web',
+    descricao: 'Aprenda as últimas tecnologias de desenvolvimento web, incluindo HTML, CSS, JavaScript e frameworks populares como Vue.js e React.',
+    duracao: 'Duração: 8 semanas'
+  },
+  {
+    titulo: 'Curso de Marketing Digital',
+    descricao: 'Descubra estratégias eficazes de marketing digital, incluindo SEO, PPC, mídias sociais, email marketing e análise de dados.',
+    duracao: 'Duração: 6 semanas'
+  },
+  {
+    titulo: 'Curso de Design Gráfico',
+    descricao: 'Domine os princípios do design gráfico, incluindo teoria das cores, tipografia, layout e ferramentas de design como Adobe Illustrator e Photoshop.',
+    duracao: 'Duração: 10 semanas'
+  }
+]);
 </script>
 
 <template>
@@ -194,6 +240,7 @@ const typedText = ref("Cursos de Curta Duração");
         <!-- Feature section -->
         <div class="relative w-full h-screen">
         <img class="w-full h-full object-cover" src="/estudante-sem-fundo.png" alt="Imagem">
+          <img src="/wave-dark-primary.svg" alt="Imagem" class="absolute inset-0 w-full h-full mt-48">
         <div class="absolute inset-0 flex justify-center items-center lg:justify-start">
           <div class="w-full h-full max-w-2xl px-6 py-8  text-white">
             <div class="lg:text-left mt-32 lg:mx-auto">
@@ -203,37 +250,47 @@ const typedText = ref("Cursos de Curta Duração");
             </div>
           </div>
         </div>
-      </div>
-
-        <div class="container relative overflow-hidden bg-white" style="height: 400px;">
-          <img src="/wave-dark-primary.svg" alt="Imagem" class="absolute inset-0 w-full h-full">
-          <div class="absolute inset-0 flex justify-center items-center">
-            <div class="py-10 px-4 text-center z-10">
-              <p>Texto no centro do container</p>
+          <div class="container relative overflow-hidden bg-white" style="height: 400px;">
+            <div class="absolute inset-0 flex justify-center items-center">
+              <div class="py-10 px-24 z-10">
+                <dl class="text hidden-scroll grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                  <div v-for="feature in features" :key="feature.name" class="relative pl-16">
+                    <dt class="text-base font-semibold leading-7 text-gray-900">
+                      <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary">
+                        <component :is="feature.icon" class="h-6 w-6 text-white" aria-hidden="true" />
+                      </div>
+                      {{ feature.name }}
+                    </dt>
+                    <dd class="mt-2 text-base leading-7 text-gray-600">{{ feature.description }}</dd>
+                  </div>
+                </dl>
+              </div>
             </div>
           </div>
-        </div>
-
-
+      </div>
         <div class="mx-auto mt-20 max-w-7xl px-6 sm:mt-56 lg:py-10 lg:px-8">
           <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-64 lg:max-w-4xl">
-            <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-              <div v-for="feature in features" :key="feature.name" class="relative pl-16">
-                <dt class="text-base font-semibold leading-7 text-gray-900">
-                  <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                    <component :is="feature.icon" class="h-6 w-6 text-white" aria-hidden="true" />
+            <div class="w-full">
+              <h2 class="text text-center text-5xl font-bold mb-10 text-primary">Cursos Disponíveis</h2>
+              <hr class="my-8 border-t-4 w-1/4 border-primary mx-auto rounded-full">
+              <div class="flex justify-between ">
+                <div v-for="(curso, index) in cursos" :key="index" class="text mt-9 w-1/3 p-4 cursor-pointer transition-transform ease-in-out transform hover:scale-105">
+                  <div class="bg-white hover:bg-primary border-primary/10 hover:text-white  rounded-lg shadow-md p-6">
+                    <h3 class="text-lg font-bold mb-2">{{ curso.titulo }}</h3>
+                    <p>{{ curso.descricao }}</p>
+                    <p class="mt-4 text-gray-500 text-sm">{{ curso.duracao }}</p>
+                    <secondary-button class="mt-5">Inscrever-se</secondary-button>
                   </div>
-                  {{ feature.name }}
-                </dt>
-                <dd class="mt-2 text-base leading-7 text-gray-600">{{ feature.description }}</dd>
+                </div>
               </div>
-            </dl>
+            </div>
+
           </div>
         </div>
       </main>
 
       <!-- Footer -->
-      <div class="mx-auto mt-32 max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto mt-32 max-w-7xl px-6 lg:px-8 bg-secondary">
         <footer aria-labelledby="footer-heading" class="relative border-t border-gray-900/10 py-24 sm:mt-56 sm:py-32">
           <h2 id="footer-heading" class="sr-only">Footer</h2>
           <div class="xl:grid xl:grid-cols-3 xl:gap-8">
@@ -241,36 +298,36 @@ const typedText = ref("Cursos de Curta Duração");
             <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
               <div class="md:grid md:grid-cols-2 md:gap-8">
                 <div>
-                  <h3 class="text-sm font-semibold leading-6 text-gray-900">Solutions</h3>
+                  <h3 class="text-sm font-semibold leading-6 text-primary">Solutions</h3>
                   <ul role="list" class="mt-6 space-y-4">
                     <li v-for="item in footerNavigation.solutions" :key="item.name">
-                      <a :href="item.href" class="text-sm leading-6 text-gray-600 hover:text-gray-900">{{ item.name }}</a>
+                      <a :href="item.href" class="text-sm leading-6 text-white hover:text-altern">{{ item.name }}</a>
                     </li>
                   </ul>
                 </div>
                 <div class="mt-10 md:mt-0">
-                  <h3 class="text-sm font-semibold leading-6 text-gray-900">Support</h3>
+                  <h3 class="text-sm font-semibold leading-6 text-primary">Support</h3>
                   <ul role="list" class="mt-6 space-y-4">
                     <li v-for="item in footerNavigation.support" :key="item.name">
-                      <a :href="item.href" class="text-sm leading-6 text-gray-600 hover:text-gray-900">{{ item.name }}</a>
+                      <a :href="item.href" class="text-sm leading-6 text-white hover:hover:text-altern">{{ item.name }}</a>
                     </li>
                   </ul>
                 </div>
               </div>
               <div class="md:grid md:grid-cols-2 md:gap-8">
                 <div>
-                  <h3 class="text-sm font-semibold leading-6 text-gray-900">Company</h3>
+                  <h3 class="text-sm font-semibold leading-6 text-primary">Company</h3>
                   <ul role="list" class="mt-6 space-y-4">
                     <li v-for="item in footerNavigation.company" :key="item.name">
-                      <a :href="item.href" class="text-sm leading-6 text-gray-600 hover:text-gray-900">{{ item.name }}</a>
+                      <a :href="item.href" class="text-sm leading-6 text-white hover:hover:text-altern">{{ item.name }}</a>
                     </li>
                   </ul>
                 </div>
                 <div class="mt-10 md:mt-0">
-                  <h3 class="text-sm font-semibold leading-6 text-gray-900">Legal</h3>
+                  <h3 class="text-sm font-semibold leading-6 text-primary">Legal</h3>
                   <ul role="list" class="mt-6 space-y-4">
                     <li v-for="item in footerNavigation.legal" :key="item.name">
-                      <a :href="item.href" class="text-sm leading-6 text-gray-600 hover:text-gray-900">{{ item.name }}</a>
+                      <a :href="item.href" class="text-sm leading-6 text-white hover:hover:text-altern">{{ item.name }}</a>
                     </li>
                   </ul>
                 </div>
