9f6b37e240e8fe414c10de25350a43cd263ad8c2
leehendrick <imhendrick16@gmail.com>
2024-04-08 14:00:18 +0100

    add landing page layout

---
+++ resources/js/Layouts/LandingLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import LandingHeader from "@/Components/LandingHeader.vue";
+import Footer from "@/Components/Footer.vue";
+</script>
+
+<template>
+  <landing-header :canLogin="$page.props.canLogin" :canRegister="$page.props.canRegister" :laravelVersion="$page.props.laravelVersion" :phpVersion="$page.props.phpVersion" />
+  <slot/>
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
