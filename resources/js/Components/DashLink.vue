4a42bf10f056576c064bbc2c42411de538d50c06
leehendrick <imhendrick16@gmail.com>
2024-04-15 10:27:27 +0100

    add custom navlink for dashboard

---
+++ resources/js/Components/DashLink.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+  href: {
+    type: String,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+  },
+});
+
+const classes = computed(() =>
+    props.active
+        ? 'bg-gray-800 text-white'
+        : 'text-gray-400 hover:text-white hover:bg-gray-800',
+        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+);
+</script>
+
+<template>
+  <Link :href="href" :class="classes">
+    <slot />
+  </Link>
+</template>
