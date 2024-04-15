fa2fd355210683c0ffa3e9ba00304f3286bc1b61
leehendrick <imhendrick16@gmail.com>
2024-04-15 10:06:16 +0100

    remove authenticated layout

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,4 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import AdminLayout from "@/Layouts/AdminLayout.vue";
 </script>
