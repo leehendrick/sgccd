013ca80171383a3f896830d445f478524790599d
leehendrick <imhendrick16@gmail.com>
2024-04-17 12:57:19 +0100

    change the active menu background color & the hover

--- resources/js/Components/DashLink.vue
+++ resources/js/Components/DashLink.vue
@@ -14,8 +14,8 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'bg-gray-800 text-white'
-        : 'text-gray-400 hover:text-white hover:bg-gray-800',
+        ? 'bg-primary text-white'
+        : 'text-gray-400 hover:text-white hover:bg-primary',
         'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
 );
 </script>
