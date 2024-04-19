77e88aa650efc5dd115162cb987173609dd9a7f9
leehendrick <imhendrick16@gmail.com>
2024-04-19 12:49:39 +0100

    add component for subscriptions gest

---
+++ resources/js/Pages/Dash/Inscrições.vue
@@ -0,0 +1,89 @@
+<script setup>
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+import {onMounted, ref} from "vue";
+import {usePage} from "@inertiajs/vue3";
+
+const { props } = usePage();
+
+const people = ref([])
+
+defineProps({
+  values: Object,
+})
+
+onMounted(() => {
+
+})
+</script>
+
+<template>
+  <admin-layout>
+    <h1>Dashboard\Inscrições</h1>
+      <div class="px-4 sm:px-6 lg:px-8 mt-10">
+        <div class="sm:flex sm:items-center">
+          <div class="sm:flex-auto">
+            <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
+            <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title,
+              email and role.</p>
+          </div>
+          <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+            <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add user</button>
+          </div>
+        </div>
+        <div class="mt-8 flow-root">
+          <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <table class="min-w-full divide-y divide-gray-300">
+                <thead>
+                <tr>
+                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Name</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 bg-white">
+                <tr v-for="person in values.data" :key="person.email">
+                  <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                    <div class="flex items-center">
+                      <div class="h-11 w-11 flex-shrink-0">
+                        <img class="h-11 w-11 rounded-full" :src="person.image" alt="" />
+                      </div>
+                      <div class="ml-4">
+                        <div class="font-medium text-gray-900">{{ person.nome }}</div>
+                        <div class="mt-1 text-gray-500">{{ person.email }}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    <div class="text-gray-900">{{ person.bi }}</div>
+                    <div class="mt-1 text-gray-500">{{ person.instituicao }}</div>
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Active</span>
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{{ person.area_formacao }}</td>
+                  <td class="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                    >Edit<span class="sr-only">, {{ person.nome }}</span></a
+                    >
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    <div class="mt-6 ml-5">
+      Paginator
+    </div>
+  </admin-layout>
+</template>
+
+<style scoped>
+
+</style>
