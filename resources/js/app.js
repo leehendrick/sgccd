aabd20d62c476b7ea0babe8dd4a0599393e1f6f6
leehendrick <imhendrick16@gmail.com>
2024-04-23 11:37:03 +0100

    update file

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,19 +10,22 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => import(`./Pages/${name}.vue`),
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .component("Link", Link)
-            .mount(el);
+            .mount(el)
     },
     progress: {
         color: '#EAB841',
         showSpinner: true,
     },
-});
+})
 
 InertiaProgress.init({
     color: '#EAB841',
