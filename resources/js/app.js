9455f9e6693f8a0d1cc00a99a38a20473b289a60
leehendrick <imhendrick16@gmail.com>
2024-04-23 10:09:05 +0100

    bug fix: real time search

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,31 +2,27 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
-import {createInertiaApp, Link} from '@inertiajs/inertia-vue3';
+import { createInertiaApp, Link } from '@inertiajs/vue3';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import {InertiaProgress} from "@inertiajs/progress";
+import { InertiaProgress } from "@inertiajs/progress";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`]
-    },
+    resolve: (name) => import(`./Pages/${name}.vue`),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .component("Link", Link)
-            .mount(el)
+            .mount(el);
     },
     progress: {
         color: '#EAB841',
         showSpinner: true,
     },
-})
+});
 
 InertiaProgress.init({
     color: '#EAB841',
