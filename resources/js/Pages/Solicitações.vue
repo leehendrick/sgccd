d58f93adcc0a55813fdddd0f9b3bcc13657a0244
leehendrick <imhendrick16@gmail.com>
2024-04-23 10:16:31 +0100

    change button

--- resources/js/Pages/Solicitações.vue
+++ resources/js/Pages/Solicitações.vue
@@ -73,7 +73,7 @@ onMounted(() => {
                   <td class="relative whitespace-nowrap px-5 py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                     <TableButton status="ver"/>
                     <TableButton status="delete"/>
-                    <TableButton status="edit"/>
+                    <TableButton status="confirm"/>
                   </td>
                 </tr>
                 </tbody>
