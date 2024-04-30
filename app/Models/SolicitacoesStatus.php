6c677e4ceca3e12b435bc20f00a420fa650c9033
leehendrick <imhendrick16@gmail.com>
2024-04-30 09:53:05 +0100

    add new models

---
+++ app/Models/SolicitacoesStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SolicitacoesStatus extends Model
+{
+    use HasFactory;
+}
