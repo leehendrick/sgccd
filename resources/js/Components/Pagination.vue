46b6d842a541cc26ccb1e3e4c04dd94b0f33afd2
leehendrick <imhendrick16@gmail.com>
2024-05-02 11:33:35 +0100

    add new style

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -2,22 +2,26 @@
   defineProps({
     links: Array
   })
+  import { ArrowLongLeftIcon, ArrowLongRightIcon } from '@heroicons/vue/20/solid'
 </script>
 
 <template>
-  <div>
-    <Component
-        :is="link.url ? 'Link' : 'span'"
-        v-for="link in links"
-        :href="link.url"
-        v-html="link.label"
-        :class="{
-          'text-gray-500' : !link.url,
-          'font-bold bg-primary text-gray-200': link.active
-        }"
-        class="px-1"
-    />
-  </div>
+  <nav class="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
+    <div class="hidden md:-mt-px md:flex">
+          <Component
+              :is="link.url ? 'a' : 'span'"
+          v-for="(link, index) in links"
+          :key="index"
+          :href="link.url"
+          v-html="link.label"
+          :class="{
+          'text-gray-500': !link.url,
+          'font-bold text-primary': link.active
+          }"
+          class="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium hover:border-primary hover:text-gray-700"
+          />
+    </div>
+  </nav>
 </template>
 
 <style scoped>
