edea0af1a6e73ad615a5652185c38ffeb75ead41
leehendrick <imhendrick16@gmail.com>
2024-05-02 09:47:52 +0100

    add courses seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory(1)->create();
+       /*  \App\Models\User::factory(1)->create();
 
          \App\Models\User::factory()->create([
              'name' => 'Hendrick Nkuba',
              'email' => 'test@example.com',
-         ]);
+         ]); */
+
+         \App\Models\Courses::factory(5)->create();
     }
 }
