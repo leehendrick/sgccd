fc647b0dfeca9ffbd49321990c72b3d40e36892e
leehendrick <imhendrick16@gmail.com>
2024-05-07 10:45:14 +0100

    remove new invoice button

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -118,10 +118,6 @@ const clients = [
                             <div class="order-last flex w-full gap-x-8 text-sm font-semibold leading-6 sm:order-none sm:w-auto sm:border-l sm:border-gray-200 sm:pl-6 sm:leading-7">
                               <a v-for="item in secondaryNavigation" :key="item.name" :href="item.href" :class="item.current ? 'text-indigo-600' : 'text-gray-700'">{{ item.name }}</a>
                             </div>
-                            <a href="#" class="ml-auto flex items-center gap-x-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                              <PlusSmallIcon class="-ml-1.5 h-5 w-5" aria-hidden="true" />
-                              New invoice
-                            </a>
                           </div>
                         </header>
 
