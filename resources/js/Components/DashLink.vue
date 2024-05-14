efe306f39e7d6a8e84308f6fe954f8bf4cc6b832
leehendrick <imhendrick16@gmail.com>
2024-05-14 10:55:00 +0100

    update: change button border to full rounded

--- resources/js/Components/DashLink.vue
+++ resources/js/Components/DashLink.vue
@@ -1,27 +1,26 @@
 <script setup>
-import { computed } from 'vue';
-import { Link } from '@inertiajs/vue3';
+import { computed } from "vue";
+import { Link } from "@inertiajs/vue3";
 
 const props = defineProps({
-  href: {
-    type: String,
-    required: true,
-  },
-  active: {
-    type: Boolean,
-  },
+    href: {
+        type: String,
+        required: true,
+    },
+    active: {
+        type: Boolean,
+    },
 });
 
 const classes = computed(() =>
     props.active
-        ? 'bg-primary text-white'
-        : 'text-gray-400 hover:text-white hover:bg-primary',
-        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+        ? "bg-primary text-white"
+        : "text-gray-400 hover:text-white hover:bg-primary",
 );
 </script>
 
 <template>
-  <Link :href="href" :class="classes">
-    <slot />
-  </Link>
+    <Link :href="href" :class="classes">
+        <slot />
+    </Link>
 </template>
