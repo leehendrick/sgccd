e5a7655d0ff11421e3b43cdc9ec18ec95dda6a8c
leehendrick <imhendrick16@gmail.com>
2024-05-14 10:55:02 +0100

    update: change button border to full rounded

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="inline-flex items-center px-4 py-2 bg-primary border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-btnSeeHover focus:outline-none transition ease-in-out duration-150"
+        class="inline-flex items-center px-4 py-2 bg-primary border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-btnSeeHover focus:outline-none transition ease-in-out duration-150"
     >
         <slot />
     </button>
