3f7b5d5e54ce38dff00bedac86aeecc379e414c4
leehendrick <imhendrick16@gmail.com>
2024-05-21 10:18:33 +0100

    add: new method to classes controller

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Classes;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ClassesController extends Controller
 {
-    public static function index()
-    {
 
+    public static function getTurmas(Request $request): \Inertia\Response
+    {
+        return Inertia::render('Turmas',[
+            'turmasValues' => Classes::query()
+                ->when($request->input('search'), function ($query, $search) {
+                    $query->where('nome', 'like', "%{$search}%");
+                })
+                ->orderByDesc('created_at', 'desc')
+                ->paginate(6)
+                ->withQueryString()
+            /**->through(fn($student) => [
+            //Aqui nós determinamos que campos da bd o props vai receber
+            ])**/
+            ,
+
+            'filters' => $request->only(['search'])
+        ]);
     }
 }
